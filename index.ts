c0659c92d514822c534b66c20f283101edcb4827
Batuta <Saelbatita@gmail.com>
2023-11-21 15:59:38 +0100

    validation de votre message

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import { ShortMessageService } from "./orange";
-const sms = new ShortMessageService("Basic RzVHdFJhcXBCeTlEYks1cVp4cU55ekdnTmt0alE5VGk6cGhPcmM2RmVBWlRUaTVvRw==","+243894437513");
-sms.sendSms("+243812167999","Salut Comment tu vas?");
+const sms = new ShortMessageService("******","+243******");
+sms.sendSms("+243*****","Salut Comment tu vas?");
--- orange.ts
+++ orange.ts
@@ -5,12 +5,25 @@ export class ShortMessageService {
     private _token: string;
     private _phoneNumber: string;
 
+    /**
+     * 
+     * @param token : votre clé qui commence par {Basic ****************}
+     * @param phoneNumber : le bumero que vous avez utiliser pour activé votre compte ORANGE SMS API
+     */
     constructor(token: string, phoneNumber: string) {
         this._token = token;
         this._phoneNumber = phoneNumber;
     }
 
+    // Méthode publique pour envoyer un SMS
+    /**
+     * 
+     * @param destinateur :vous devez mettre le numero de la personne que vous voulez envoyer le SMS 
+     * sous le format: {+243 812167999}
+     * @param message : vous devez mettre le message 
+     */
     public sendSms(destinateur: string, message: string) {
+        // Configuration de l'authentification
         let credentials: string = this._token;
         let postData: string = "grant_type=client_credentials";
         let options: https.RequestOptions = {
@@ -23,6 +36,8 @@ export class ShortMessageService {
                 'Content-Length': Buffer.byteLength(postData).toString()
             }
         };
+
+        // Effectuer la demande pour obtenir le jeton d'accès
         let req = https.request(options, (response) => {
             response.setEncoding('utf8');
             let responseData = '';
@@ -31,6 +46,7 @@ export class ShortMessageService {
             });
             response.on('end', () => {
                 let result = JSON.parse(responseData);
+                // Une fois le jeton d'accès obtenu, appeler la méthode privée send pour envoyer le SMS
                 this.send(result.access_token, destinateur, message);
             });
         }).on('error', (e) => { });
@@ -38,6 +54,7 @@ export class ShortMessageService {
         req.end();
     }
 
+    // Méthode privée pour envoyer le SMS une fois le jeton d'accès obtenu
     private send(token: string, destinateur: string, message: string) {
         let receiver = "tel:" + destinateur;
         let sender = `tel:${this._phoneNumber}`;
@@ -45,6 +62,8 @@ export class ShortMessageService {
             'Authorization': "Bearer " + token,
             'Content-Type': 'application/json'
         };
+
+        // Corps de la requête pour envoyer le SMS
         let body = {
             outboundSMSMessageRequest: {
                 address: receiver,
@@ -55,7 +74,7 @@ export class ShortMessageService {
             }
         };
 
-
+        // Options de la requête HTTP pour envoyer le SMS
         let options = {
             uri: `https://api.orange.com/smsmessaging/v1/outbound/tel:${this._phoneNumber}/requests`,
             method: 'POST',
@@ -63,14 +82,15 @@ export class ShortMessageService {
             body: JSON.stringify(body)
         };
 
-        request(options, (error:any, response:any) => {
+        // Effectuer la requête pour envoyer le SMS
+        request(options, (error: any, response: any) => {
             if (!error && response.statusCode == 201) {
+                // Si la requête est réussie, afficher le code de statut
                 console.log(response.statusCode);
             } else {
+                // En cas d'erreur, afficher l'erreur
                 console.log(error);
             }
         });
-
     }
 }
-
